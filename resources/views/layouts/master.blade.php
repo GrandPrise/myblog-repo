acb123911b142ae27677a2db931ec17ac6027615
GrandPrise <diyaehyrakce@gmail.com>
2022-08-26 23:42:37 +0100

    inegrated template + added roles

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Blog @yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+    
+    
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    @include('layouts.inc.admin-navbar')
+
+    <div id="layoutSidenav">
+        @include('layouts.inc.admin-sidebar')
+
+        <div id="layoutSidenav_content">
+            <main>
+                @yield('content')
+            </main>
+            @include('layouts.inc.admin-footer')
+        </div>
+    </div>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}" ></script>
+    <script src="{{ asset('assets/js/scripts.js') }}" ></script>
+</body>
+</html>
